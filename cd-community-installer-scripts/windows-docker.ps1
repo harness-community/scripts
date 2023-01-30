3ef8552f4fffd0971af036a7fd418dca423f735e
rkapoor10 <ritik.kapoor@harness.io>
2023-01-30 18:21:31 +0530

    update: script with functions

--- cd-community-installer-scripts/windows-docker.ps1
+++ cd-community-installer-scripts/windows-docker.ps1
@@ -1,25 +1,17 @@
-$cpus = Get-WmiObject -Class Win32_Processor | Measure-Object -Property NumberOfCores -Sum
-$memoryObject = Get-WmiObject -Class Win32_ComputerSystem 
-$totalMemory = [math]::Round($memoryObject.TotalPhysicalMemory / 1GB, 2) 
-
-Write-Host "-----------SYSTEM RESOURCES-----------------"
-Write-Host "CPUs : $($cpus.Sum)"
-Write-Host "Total RAM : $totalMemory GB"
-
-
-if ($totalMemory -lt 3 ) {
-    Write-Host "Minimum 3GB RAM is required $($totalMemory)GB RAM available"
+function Check-Resources {
+    $cpus = Get-WmiObject -Class Win32_Processor | Measure-Object -Property NumberOfCores -Sum
+    $memoryObject = Get-WmiObject -Class Win32_ComputerSystem 
+    $totalMemory = [math]::Round($memoryObject.TotalPhysicalMemory / 1GB, 2) 
+    if (!($totalMemory -lt 3 -and $cpus.Sum -lt 2)) {
+        return $true
+    }
+    return $false
+    
 }
-if ($cpus.Sum -lt 2) {
-    Write-Host "Minimum of 2CPUs are required $($cpus.Sum)CPUs are available"
-}
-else {
-    Write-Host "System Resource are sufficient"
-    Write-Host "Minimum Requirements"
-    Write-Host "RAM : 3GB | CPUs : 2"
-    # check for wsl install 
+
+function Get-WSL {
     # Install WSL only if not enabled already
-    Write-Host "Run this script as administrator"
+    # Write-Host "Run this script as administrator"
     if (!(Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux)) {
         # Enable WSL
         Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
@@ -28,9 +20,11 @@ else {
     else {
         Write-Host "Windows Subsystem for Linux (WSL) is already enabled."
     }
+}
 
-    # Check if Docker is already installed
-    if (!(Get-Command "docker")) {
+function Get-Docker {
+     # Check if Docker is already installed
+     if (!(Get-Command "docker")) {
         # Download Docker for Windows installer
         Write-Host "Downloading Docker for Windows installer..."
         Write-Host "After installation restart the system and re-run the script to install Harness CD Community"
@@ -47,14 +41,16 @@ else {
         }
         else { Write-Host "Docker for Windows is already installed." }   
     }
+}
 
+
+function Get-Gitbash {
     # Check if Git Bash is already installed
     if (!(Test-Path "C:\Program Files (x86)\Git\git-bash.exe")) {
         # Download Git Bash installer
         Write-Host "Downloading Git Bash installer..."
         $gitInstaller = "$env:TEMP\GitBash.exe"
         Invoke-WebRequest "https://github.com/git-for-windows/git/releases/download/v2.30.2.windows.1/Git-2.30.2-32-bit.exe" -OutFile $gitInstaller
-
         # Install Git Bash
         Write-Host "Installing Git Bash..."
         Start-Process -FilePath $gitInstaller -ArgumentList "/SILENT" -Wait
@@ -63,9 +59,9 @@ else {
     else {
         Write-Host "Git Bash is already installed."
     }
+}
 
-    # script for Harness CD Community Edition Setup
-    if ((Get-Command "docker") -and (Test-Path "C:\Program Files (x86)\Git\git-bash.exe")) {
+function Clone-CDCommunity {
         $cloneDestination = Read-Host -Prompt "Enter the destination folder for cloning Harness CD Community Git Repo"
         $repoLink = "https://github.com/harness/harness-cd-community.git"
         Set-Location $cloneDestination
@@ -84,6 +80,25 @@ else {
             Write-Host "Some error occured IN harness CD Setup"
         }
         
+    
+}
+
+if(Check-Resources){
+    Get-WSL
+    Get-Docker
+    Get-Gitbash
+    if ((Get-Command "docker") -and (Test-Path "C:\Program Files (x86)\Git\git-bash.exe")) {
+    Clone-CDCommunity}
+    else{
+        Write-Host "Setup Failed!"
     }
 
-} 
+
+}
+else{
+    Write-Host "Insufficient Resources! Harness CD Community requires a minimum of 3GB RAM and 2 CPU"
+}
+
+
+
+
